87d91292b2d98bc49c2e562f8ece330cc65a2b30
nishant garg <nishant.garg@tothenew.com>
2025-03-03 10:18:38 +0530

    updated code

--- src/main/java/org/example/Locator.java
+++ src/main/java/org/example/Locator.java
@@ -5,19 +5,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locator {
+
+   // public void register
+
     public static void main(String[] args ) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(new By.ById("inputUsername")).sendKeys("Nishant");
+
         driver.findElement(By.name("inputPassword")).sendKeys("123456789");
         driver.findElement(By.id("chkboxOne")).click();
+        // to check on any checkbox we use click methode.
         driver.findElement(By.className("signInBtn")).click();
         driver.findElement(By.id("chkboxTwo")).click();
         //to fetch error message we need to stop main thread.
         Thread.sleep(1000);
         //error message need time
-        System.out.println(driver.findElement(By.className("error")).getText());
+      String errorMessage =  driver.findElement(By.className("error")).getText();
+      if(!errorMessage.isEmpty()){
+
+          driver.findElement(By.id("visitUsTwo")).click();
+          String userName = driver.findElement(By.id("inputUsername")).getText();
+          String Password = driver.findElement(By.name("inputPassword")).getText();
+          driver.get(driver.findElement(By.cssSelector(".top-right :nth-child(2) >a.theme-btn")).getText());
+      }
+      driver.quit();
 
     }
+
 }
